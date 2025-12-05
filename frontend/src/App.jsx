c3ee5b0d037f0d36f2b936703848367e8990cb8b
nikunjsrivastav <nikunjshrivastav47@gmail.com>
2025-12-06 00:00:32 +0530

    Changes to app and css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [query, setQuery] = useState("");
   return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>MovieTracker</h1>
-      <p>Your AI Movie Recommendation Website</p>
+    <div className="container">
+      <h1 className="title">MovieTracker</h1>
+      <p className="subtitle">Your AI Movie Recommendation Website</p>
+      <div className="searchBox">
+        <input type="text" placeholder="Enter the movie name... " onChange={(e) => setQuery(e.target.value)} className="searchInput"/>
+        <button onClick={()=>alert("Searching for: "+query)} className="searchButton">Search</button>
+      </div>
+      <p className="typedText">You typed: {query}</p>
     </div>
   );
 }
